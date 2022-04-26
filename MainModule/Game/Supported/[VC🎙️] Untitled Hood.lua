294b45dbc1cbd4cc0af2bf7e3b77941e0861f3d1
9XavierX <104410995+9XavierX@users.noreply.github.com>
2022-04-26 16:39:04 -0700

    Game
    https://web.roblox.com/games/9183932460/VC-Untitled-Hood

---
+++ MainModule/Game/Supported/[VC🎙️] Untitled Hood.lua
@@ -0,0 +1,3 @@
+if game.PlaceId = 9183932460 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/9XavierX/Lucid-V/main/MainScript.lua?token=GHSAT0AAAAAABT56TSEAZJLS6YPYNFVFPEGYTIQFXQ'))()
+print("Game detected: Untitled Hood")

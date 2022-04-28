d53b2fb623935083cf7fc1fece704c686a8037d8
9XavierX <104410995+9XavierX@users.noreply.github.com>
2022-04-27 18:33:30 -0700

    Update TrashTalk.lua

--- MainModule/Game/Supported/Supported-2/TrashTalk.lua
+++ MainModule/Game/Supported/Supported-2/TrashTalk.lua
@@ -1,15 +1,18 @@
 local words = {
         'ur bad🤢🤢🤮🤮',
         'seed',
-        'im not hacking ur just bad',
+        'im not hacking ur just bad🧑‍💻',
         'clown🤡🤡',
         'sonned',
+        'Lucid V on top💫',
         'how did u miss that many shots',
         'worser than my grandma',
         'imagine dying in a lego game 😂',
         'you use jjsploit 😂',
         'you better run',
-        'Lucid V on top💫'
+        'Lucid V on top💫',
+        'i dont care that im hacking😂',
+        'i use an alt so idc if i get banned 😎'
 }
 
 
